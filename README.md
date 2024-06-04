9ee6dafbd043af70dced2ee22d5d90eb38a74f6b
maxgallup <maxgallup@pm.me>
2024-06-04 22:20:47 +0200

    chore: added readme notes and libs.txt

--- README.md
+++ README.md
@@ -1,22 +1,54 @@
 # fuzz-project 
 
 
+The following is a list of notes for deciding on a target.
+
+* open type format otf
 * true type format (ttf)
     * standard: https://en.wikipedia.org/wiki/OpenType
     * library: freetype linux implementation (shipped with most distros) https://en.wikipedia.org/wiki/FreeType
     * library: gltt opengl https://directory.fsf.org/wiki/GLTT
     * library: sdl_ttf for games https://github.com/libsdl-org/SDL_ttf
     * library: tool https://github.com/fonttools/fonttools
-
-
+    * closed source lib: Apple CoreGraphics implementation
+    * the following formats build on top of ttf by adding their own optimization:
+        * .woff, .woff2, .eot
+    * obsolete file formats (replaced by ttf and otf)
+        * .fnt, 
     * font embedding (into pdfs, word documents...) https://en.wikipedia.org/wiki/Font_embedding
-    * can we use an LLM for fuzzing the ttf VM?
+    * existing research:
+        * https://github.com/googleprojectzero/BrokenType
+        * https://immunityproducts.blogspot.com/2013/03/infiltrate-preview-truetype-font.html
+        * https://googleprojectzero.blogspot.com/2016/06/a-year-of-windows-kernel-font-fuzzing-1_27.html
+        * https://googleprojectzero.blogspot.com/2016/07/a-year-of-windows-kernel-font-fuzzing-2.html
+        * https://github.com/signalsec/kirlangic-ttf-fuzzer
+        * https://www.fortinet.com/blog/threat-research/a-14-day-journey-through-embedded-open-type-font-fuzzing
+
+* other font types
+    * .bdf and .pcf are used in XOrg Window server
+    * .pfa printer specific fonts
+    * .pf2 could be interesting: used by grub-mkfont (part of grub) https://elixir.bootlin.com/grub/2.00/source/util/grub-mkfont.c
+    * .abf a binary format used by Adobe applications.
+    * .sfd a database representation Used by FontForge, an open-source font editor
+    * .VFB (FontLab Studio Font File): Specific to FontLab Studio, a professional font editing software.
+    * .VLW (Processing Font File): Used in Processing, a flexible software sketchbook and a language for learning how to code within the context of visual arts.
+    * .JFPROJ (JSON Font Project File): A JSON-based format for storing font projects.
+    * .GLIF (Glyph Interchange Format File): Designed for exchanging vector graphics between applications.
+    * .UFO (Unified Font Object File): Used by Glyphs, a popular font editor.
+    
 
 * android IMs
     * LINE
 
 * linux IMs and office apps
-    * (not exactly for fuzzing) scan AppImages easily for vulnerable libs
+
+
+# Current Approach and Reasoning
+* interested in software that performs parsing like fonts, images, audio, etc...
+* enumerated an android IMs, automated library traversal for linux apps
+* researching TTF and OTF implementations
+* looking at how we can build on top of existing ttf research
+* enumerating other font type targets
 
 
 
--- linux-appimages/appimage-extractor.py
+++ linux-appimages/appimage-extractor.py
@@ -13,10 +13,10 @@ from dataclasses import dataclass
 
 # Where to store library files of each appimage, will be organized 
 # via subdirectories of the appimage's name
-OUTPUT_DIR = "/tmp/appimage-libs"
+OUTPUT_DIR = "/home/max/oss/appimage-libs"
 
 # temporary directory used for downloads
-TEMP_DIR = "/tmp/appimage-extractor"
+TEMP_DIR = "/home/max/oss/appimage-extractor"
 
 
 @dataclass
@@ -34,13 +34,13 @@ APPIMAGES = [
     AppImage("https://getsession.org/linux", f"{OUTPUT_DIR}/session", f"{TEMP_DIR}/session/session.AppImage", f"{TEMP_DIR}/session", f"{TEMP_DIR}/session/mount", "", ""),
     AppImage("https://github.com/Acreom/releases/releases/download/v1.20.1/acreom-1.20.1.AppImage", f"{OUTPUT_DIR}/acreom", f"{TEMP_DIR}/acreom/acreom.AppImage", f"{TEMP_DIR}/acreom", f"{TEMP_DIR}/acreom/mount", "", ""),
     AppImage("https://github.com/bugbakery/audapolis/releases/download/v0.3.0/audapolis-linux-x86_64-0.3.0_4.AppImage", f"{OUTPUT_DIR}/audapolis", f"{TEMP_DIR}/audapolis/audapolis.AppImage", f"{TEMP_DIR}/audapolis", f"{TEMP_DIR}/audapolis/mount", "", ""),
-    AppImage("https://github.com/AppFlowy-IO/AppFlowy/releases/download/0.5.8/AppFlowy-0.5.8-linux-x86_64.AppImage", f"{OUTPUT_DIR}/appflowy", f"{TEMP_DIR}/appflowy/appflowy.AppImage", f"{TEMP_DIR}/appflowy", f"{TEMP_DIR}/appflowy/mount", "", ""),
+    # AppImage("https://github.com/AppFlowy-IO/AppFlowy/releases/download/0.5.8/AppFlowy-0.5.8-linux-x86_64.AppImage", f"{OUTPUT_DIR}/appflowy", f"{TEMP_DIR}/appflowy/appflowy.AppImage", f"{TEMP_DIR}/appflowy", f"{TEMP_DIR}/appflowy/mount", "", ""),
     AppImage("https://github.com/allusion-app/Allusion/releases/download/v1.0.0-rc.10/Allusion-1.0.0-rc.10.AppImage", f"{OUTPUT_DIR}/allusion", f"{TEMP_DIR}/allusion/allusion.AppImage", f"{TEMP_DIR}/allusion", f"{TEMP_DIR}/allusion/mount", "", ""),
     AppImage("https://github.com/Beaver-Notes/Beaver-Notes/releases/download/3.2.0/Beaver-notes-3.2.0.AppImage", f"{OUTPUT_DIR}/beavernotes", f"{TEMP_DIR}/beavernotes/beavernotes.AppImage", f"{TEMP_DIR}/beavernotes", f"{TEMP_DIR}/beavernotes/mount", "", ""),
     AppImage("https://github.com/bdTechies/book-manager/releases/download/v1.0.1/book-manager-1.0.1-x86_64.AppImage", f"{OUTPUT_DIR}/bookmanager", f"{TEMP_DIR}/bookmanager/bookmanager.AppImage", f"{TEMP_DIR}/bookmanager", f"{TEMP_DIR}/bookmanager/mount", "", ""),
     AppImage("https://github.com/KushagraKarira/calibre-appimage/releases/download/main-0ccac329/Calibre-7.11.0-x86_64.AppImage", f"{OUTPUT_DIR}/calibre", f"{TEMP_DIR}/calibre/calibre.AppImage", f"{TEMP_DIR}/calibre", f"{TEMP_DIR}/calibre/mount", "", ""),
     AppImage("https://github.com/Bin-Huang/chatbox/releases/download/v1.3.10/Chatbox-1.3.10-x86_64.AppImage", f"{OUTPUT_DIR}/chatbox", f"{TEMP_DIR}/chatbox/chatbox.AppImage", f"{TEMP_DIR}/chatbox", f"{TEMP_DIR}/chatbox/mount", "", ""),
-    AppImage("https://github.com/giuspen/cherrytree/releases/download/v1.1.2/CherryTree-1.1.2-x86_64.AppImage", f"{OUTPUT_DIR}/cherrytree", f"{TEMP_DIR}/cherrytree/cherrytree.AppImage", f"{TEMP_DIR}/cherrytree", f"{TEMP_DIR}/cherrytree/mount", "", ""),
+    # AppImage("https://github.com/giuspen/cherrytree/releases/download/v1.1.2/CherryTree-1.1.2-x86_64.AppImage", f"{OUTPUT_DIR}/cherrytree", f"{TEMP_DIR}/cherrytree/cherrytree.AppImage", f"{TEMP_DIR}/cherrytree", f"{TEMP_DIR}/cherrytree/mount", "", ""),
     AppImage("https://github.com/SZinedine/DeepTags/releases/download/0.8.0/DeepTags_0.8.0-x86_64.AppImage", f"{OUTPUT_DIR}/deeptags", f"{TEMP_DIR}/deeptags/deeptags.AppImage", f"{TEMP_DIR}/deeptags", f"{TEMP_DIR}/deeptags/mount", "", ""),
     AppImage("https://github.com/hkdb/Densify/releases/download/v0.3.1/Densify-v0.3.1-x86_64.AppImage", f"{OUTPUT_DIR}/densify", f"{TEMP_DIR}/densify/densify.AppImage", f"{TEMP_DIR}/densify", f"{TEMP_DIR}/densify/mount", "", ""),
     AppImage("https://github.com/jimevins/glabels-qt/releases/download/glabels-3.99-master564/glabels-3.99-master564-x86_64.AppImage", f"{OUTPUT_DIR}/glabels", f"{TEMP_DIR}/glabels/glabels.AppImage", f"{TEMP_DIR}/glabels", f"{TEMP_DIR}/glabels/mount", "", ""),
@@ -59,7 +59,7 @@ APPIMAGES = [
     AppImage("https://github.com/streetwriters/notesnook/releases/download/v3.0.8/notesnook_linux_x86_64.AppImage", f"{OUTPUT_DIR}/notesnook", f"{TEMP_DIR}/notesnook/notesnook.AppImage", f"{TEMP_DIR}/notesnook", f"{TEMP_DIR}/notesnook/mount", "", ""),
     AppImage("https://github.com/vkbo/novelWriter/releases/download/v2.5b1/novelWriter-2.5b1.AppImage", f"{OUTPUT_DIR}/novelwriter", f"{TEMP_DIR}/novelwriter/novelwriter.AppImage", f"{TEMP_DIR}/novelwriter", f"{TEMP_DIR}/novelwriter/mount", "", ""),
     AppImage("https://github.com/Lunarequest/NightPDF/releases/download/v3.0.0-beta4/NightPDF-3.0.0-beta4-x86_64.AppImage", f"{OUTPUT_DIR}/nightpdf", f"{TEMP_DIR}/nightpdf/nightpdf.AppImage", f"{TEMP_DIR}/nightpdf", f"{TEMP_DIR}/nightpdf/mount", "", ""),
-    AppImage("https://github.com/felipefacundes/okular-appimage/releases/download/v22.08.1-Full_All_LIBS/Okular.full-libs-22.08.1-2-x86_64.AppImage", f"{OUTPUT_DIR}/okular", f"{TEMP_DIR}/okular/okular.AppImage", f"{TEMP_DIR}/okular", f"{TEMP_DIR}/okular/mount", "", ""),
+    # AppImage("https://github.com/felipefacundes/okular-appimage/releases/download/v22.08.1-Full_All_LIBS/Okular.full-libs-22.08.1-2-x86_64.AppImage", f"{OUTPUT_DIR}/okular", f"{TEMP_DIR}/okular/okular.AppImage", f"{TEMP_DIR}/okular", f"{TEMP_DIR}/okular/mount", "", ""),
     AppImage("https://github.com/bgallois/OpenJournal/releases/download/v1.3.4/OpenJournal-x86_64.AppImage", f"{OUTPUT_DIR}/openjournal", f"{TEMP_DIR}/openjournal/openjournal.AppImage", f"{TEMP_DIR}/openjournal", f"{TEMP_DIR}/openjournal/mount", "", ""),
     AppImage("https://github.com/GlitterWare/Passy/releases/download/v1.8.0/Passy-v1.8.0-x86-64.AppImage", f"{OUTPUT_DIR}/passy", f"{TEMP_DIR}/passy/passy.AppImage", f"{TEMP_DIR}/passy", f"{TEMP_DIR}/passy/mount", "", ""),
     AppImage("https://github.com/dragotin/pdfquirk/releases/download/v0.95/PDFQuirk-continuous-x86_64.AppImage", f"{OUTPUT_DIR}/pdfquirk", f"{TEMP_DIR}/pdfquirk/pdfquirk.AppImage", f"{TEMP_DIR}/pdfquirk", f"{TEMP_DIR}/pdfquirk/mount", "", ""),
@@ -68,9 +68,9 @@ APPIMAGES = [
     AppImage("https://github.com/pbek/QOwnNotes/releases/download/v24.6.0/QOwnNotes-x86_64.AppImage", f"{OUTPUT_DIR}/qownnotes", f"{TEMP_DIR}/qownnotes/qownnotes.AppImage", f"{TEMP_DIR}/qownnotes", f"{TEMP_DIR}/qownnotes/mount", "", ""),
     AppImage("https://github.com/qpdf/qpdf/releases/download/v11.9.0/qpdf-11.9.0-x86_64.AppImage", f"{OUTPUT_DIR}/qpdf", f"{TEMP_DIR}/qpdf/qpdf.AppImage", f"{TEMP_DIR}/qpdf", f"{TEMP_DIR}/qpdf/mount", "", ""),
     AppImage("https://github.com/harshkhandeparkar/rainbow-board/releases/download/v0.8.1/Rainbow-Board-0.8.1.AppImage", f"{OUTPUT_DIR}/rainbowboard", f"{TEMP_DIR}/rainbowboard/rainbowboard.AppImage", f"{TEMP_DIR}/rainbowboard", f"{TEMP_DIR}/rainbowboard/mount", "", ""),
-    AppImage("https://recollectr.io/download/linux/", f"{OUTPUT_DIR}/recollectr", f"{TEMP_DIR}/recollectr/recollectr.AppImage", f"{TEMP_DIR}/recollectr", f"{TEMP_DIR}/recollectr/mount", "", ""),
-    AppImage("https://github.com/saber-notes/saber/releases/download/v0.23.2/Saber-0.23.2-x86_64.AppImage", f"{OUTPUT_DIR}/saber", f"{TEMP_DIR}/saber/saber.AppImage", f"{TEMP_DIR}/saber", f"{TEMP_DIR}/saber/mount", "", ""),
-    AppImage("https://github.com/rhaberkorn/sciteco/releases/download/nightly/sciteco-gtk_nightly_x86_64.AppImage", f"{OUTPUT_DIR}/sciteco", f"{TEMP_DIR}/sciteco/sciteco.AppImage", f"{TEMP_DIR}/sciteco", f"{TEMP_DIR}/sciteco/mount", "", ""),
+    # AppImage("https://recollectr.io/download/linux/", f"{OUTPUT_DIR}/recollectr", f"{TEMP_DIR}/recollectr/recollectr.AppImage", f"{TEMP_DIR}/recollectr", f"{TEMP_DIR}/recollectr/mount", "", ""),
+    # AppImage("https://github.com/saber-notes/saber/releases/download/v0.23.2/Saber-0.23.2-x86_64.AppImage", f"{OUTPUT_DIR}/saber", f"{TEMP_DIR}/saber/saber.AppImage", f"{TEMP_DIR}/saber", f"{TEMP_DIR}/saber/mount", "", ""),
+    # AppImage("https://github.com/rhaberkorn/sciteco/releases/download/nightly/sciteco-gtk_nightly_x86_64.AppImage", f"{OUTPUT_DIR}/sciteco", f"{TEMP_DIR}/sciteco/sciteco.AppImage", f"{TEMP_DIR}/sciteco", f"{TEMP_DIR}/sciteco/mount", "", ""),
     AppImage("https://github.com/standardnotes/desktop/releases/download/v3.22.11/standard-notes-3.22.11-linux-x86_64.AppImage", f"{OUTPUT_DIR}/standardnotes", f"{TEMP_DIR}/standardnotes/standardnotes.AppImage", f"{TEMP_DIR}/standardnotes", f"{TEMP_DIR}/standardnotes/mount", "", ""),
     AppImage("https://github.com/josippapez/StoRadit/releases/download/v1.0.5/StoRadit-1.0.5.AppImage", f"{OUTPUT_DIR}/storadit", f"{TEMP_DIR}/storadit/storadit.AppImage", f"{TEMP_DIR}/storadit", f"{TEMP_DIR}/storadit/mount", "", ""),
     AppImage("https://github.com/SimonBrandner/TaskizerDesktop/releases/download/v2.0.0-rc.2/taskizer-2.0.0-rc.2-linux-x86_64.AppImage", f"{OUTPUT_DIR}/taskizer", f"{TEMP_DIR}/taskizer/taskizer.AppImage", f"{TEMP_DIR}/taskizer", f"{TEMP_DIR}/taskizer/mount", "", ""),
@@ -84,10 +84,10 @@ APPIMAGES = [
 
 def extract_lib(appimage):
     appimage.iso_offset = subprocess.run([appimage.executable_path, "--appimage-offset"], stdout=subprocess.PIPE).stdout.decode('utf-8').split(" ")[0].strip()
-    print(f">> Found offset of {appimage.iso_offset} for appimage: {appimage.executable_path}")
+    # print(f">> Found offset of {appimage.iso_offset} for appimage: {appimage.executable_path}")
     
     subprocess.run(["sudo", "mount", appimage.executable_path, appimage.mount_path, "-o", f"offset={appimage.iso_offset}"])
-    print(f">> mounted at {appimage.mount_path}")
+    # print(f">> mounted at {appimage.mount_path}")
 
     for file in glob.glob(f"{appimage.mount_path}/*.so"):
         shutil.copy(file, appimage.output_path)
@@ -107,7 +107,7 @@ def extract_lib(appimage):
     for file in glob.glob(f"{appimage.mount_path}/usr/local/lib/*"):
         shutil.copy(file, appimage.output_path)
 
-    print(f">> copied libraries to {appimage.output_path}")
+    # print(f">> copied libraries to {appimage.output_path}")
 
     subprocess.run(["sudo", "umount", appimage.mount_path])
 
@@ -116,7 +116,7 @@ def download_appimage(appimage):
         print(f"skipping {appimage.directory_path} already exists")
         return
 
-    print(f"Downloading {appimage.url} to {appimage.directory_path}")
+    print(f"Downloading {appimage.output_path.split("/")[-1]}")
     os.mkdir(appimage.output_path)
     os.mkdir(appimage.directory_path)
     os.mkdir(appimage.mount_path)
@@ -132,6 +132,7 @@ def download_appimage(appimage):
 def run(appimage):
     download_appimage(appimage)
     extract_lib(appimage)
+    print(f">> Finished {appimage.output_path.split("/")[-1]}")
 
 
 
