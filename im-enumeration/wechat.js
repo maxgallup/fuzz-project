6bb257814ed35170443448c3818add930b2cc8aa
alexbirnberg <76245883+alexbirnberg@users.noreply.github.com>
2024-06-07 10:04:13 +0200

    Update wechat.js

--- im-enumeration/wechat.js
+++ im-enumeration/wechat.js
@@ -1,30 +1,57 @@
 Java.perform(function() {
   var Runtime = Java.use("java.lang.Runtime");
 var arr = new Array();
+var callCounters = {};
 
 console.log("");
 
-Runtime.loadLibrary0.overload('java.lang.Class', 'java.lang.String').implementation = function(classObj, libName) {
+function traceLibrary(libName) {
   if (arr.indexOf(libName) == -1) {
-    console.log("Loading lib" + libName + ".so");
+    var libFullName = "lib" + libName + ".so";
+    console.log("Loading " + libFullName);
     arr.push(libName);
+    var exports = Module.enumerateExports(libFullName);
+    for (var exp of exports) {
+      if (exp.type === 'function') {
+        if (!callCounters[exp.name]) {
+                      callCounters[exp.name] = 0;
+                  }
+        
+        // Intercept and log each call to the function
+        console.log("    " + exp.name);
+        try {
+          Interceptor.attach(exp.address, {
+            onEnter: function(args) {
+              if (callCounters[exp.name] < 10) {
+                console.log("Called " + exp.name + " in " + libFullName);
+                callCounters[exp.name]++;
+              }
+            }
+          });
+        }
+        catch (e) {
+          console.log("BRR " + e);
+        }
+      }
+    }  
   }
-  return this.loadLibrary0(classObj, libName);
+}
+
+Runtime.loadLibrary0.overload('java.lang.Class', 'java.lang.String').implementation = function(classObj, libName) {
+  var ret = this.loadLibrary0(classObj, libName);
+  traceLibrary(libName);
+  return ret;
 }
 
 Runtime.loadLibrary0.overload('java.lang.ClassLoader', 'java.lang.String').implementation = function(classLoader, libName) {
-  if (arr.indexOf(libName) == -1) {
-    console.log("Loading lib" + libName + ".so");
-    arr.push(libName);
-  }
-  return this.loadLibrary0(classLoader, libName);
+  var ret = this.loadLibrary0(classLoader, libName);
+  traceLibrary(libName);
+  return ret;
 }
 
 Runtime.loadLibrary0.overload('java.lang.ClassLoader', 'java.lang.Class', 'java.lang.String').implementation = function(classLoader, classObj, libName) {
-  if (arr.indexOf(libName) == -1) {
-    console.log("Loading lib" + libName + ".so");
-    arr.push(libName);
-  }
-  return this.loadLibrary0(classLoader, classObj, libName);
+  var ret = this.loadLibrary0(classLoader, classObj, libName);
+  traceLibrary(libName);
+  return ret;
 }
 });
