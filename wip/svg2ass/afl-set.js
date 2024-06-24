9ac4ffd2c8f68f5846b3fddc19f106e8863bea93
Giovanni Di Santi <giovanni.disanti@protonmail.com>
2024-06-24 16:14:24 +0200

    wip: svg2ass state reader

--- wip/svg2ass/afl-set.js
+++ wip/svg2ass/afl-set.js
@@ -1,27 +1,40 @@
-Afl.print('******************');
-Afl.print('* AFL FRIDA MODE *');
-Afl.print('******************');
-Afl.print('');
+// Afl.print('******************');
+// Afl.print('* AFL FRIDA MODE *');
+// Afl.print('******************');
+// Afl.print('');
+// 
+// Afl.print(`PID: ${Process.id}`);
+// 
+// const main = DebugSymbol.fromName('main').address;
+// Afl.print(`main: ${main}`);
+// 
+// var states = [];
+// var node_types = [];
+// var node_names = [];
 
-Afl.print(`PID: ${Process.id}`);
 
-const main = DebugSymbol.fromName('main').address;
-Afl.print(`main: ${main}`);
+// Interceptor.attach(ptr('0x401A4A'), function(args) {
+//     var edx = this.context.rdx & 0xffffffff;
+//     Afl.print(`edx: ${edx}`);
+//     transitions.push(edx);
+//     if (transitions.length > 4) {
+//         let val = 0;
+//         for (let i = 0; i < 5; i++) {
+//             val = Afl.IJON.hashint(val, transitions[i]);
+//         }
+//         Afl.print(`transitions: ${transitions}, val: ${val}`);
+//         Afl.IJON.map_set(this.context, val);
+//     }
+// });
 
-var transitions = [];
-
-Interceptor.attach(ptr('0x401A4A'), function(args) {
-    var edx = this.context.rdx & 0xffffffff;
-    Afl.print(`edx: ${edx}`);
-    transitions.push(edx);
-    if (transitions.length > 4) {
-        let val = 0;
-        for (let i = 0; i < 5; i++) {
-            val = Afl.IJON.hashint(val, transitions[i]);
-        }
-        Afl.print(`transitions: ${transitions}, val: ${val}`);
-        Afl.IJON.map_set(this.context, val);
-    }
+Interceptor.attach(ptr('0x407FAD'), function(args) {
+    var rbp = this.context.rbp;
+    var node_name_addr = Memory.readU64(rbp.sub(0x38));
+    var state_addr = rbp.sub(0x54);
+    var node_name = ptr(node_name_addr).readUtf8String();
+    var state = Memory.readS32(state_addr);
+    Afl.print(`node_name_addr: ${node_name_addr}, node_name: ${node_name} | state_addr: ${state_addr}, state: ${state}`);
+    // console.log(`node_name_addr: ${node_name_addr}, node_name: ${node_name} | state_addr: ${state_addr}, state: ${state}`);
 });
 
 Afl.done();
