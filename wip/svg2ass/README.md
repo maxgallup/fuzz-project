da735f3c94a01ea1dfbc1da9b92379b240227283
Giovanni Di Santi <giovanni.disanti@protonmail.com>
2024-06-24 18:23:35 +0200

    wip: svg2ass better test crashes new stats

--- wip/svg2ass/README.md
+++ wip/svg2ass/README.md
@@ -3,8 +3,15 @@
 ## Test Crash
 
 ```
-./binaries/svg2ass_sanitized < ./io/output_dir_<something>/default.crashes/<crash_file>
+./binaries/svg2ass_sanitized < ./io/output_dir_<something>/default/crashes/<crash_file>
 ```
 
 Run ./test_crash.sh (setting the right output directory of the crashes) to analyze
-the `unique` ones.
+the `unique` crashes.
+
+Example:
+
+```
+./test_crash.sh ./io/output_dir_frida_set/default/crashes/
+```
+
--- wip/svg2ass/test_crash.sh
+++ wip/svg2ass/test_crash.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# CRASH_DIR="./io/output_dir_frida_inc2/default/crashes"
-# CRASH_DIR="./io/output_dir_frida_set1/default/crashes"
-CRASH_DIR="./io/output_dir_frida_normal0/default/crashes"
-BINARY="./binaries/svg2ass/svg2ass_sanitized"
+CRASH_DIR=$1
+BINARY="./binaries/svg2ass_sanitized"
 LOG_DIR="./asan_logs"
 
 mkdir -p $LOG_DIR
 rm ./asan_logs/*
-
 
 counter=1
 
@@ -21,9 +18,6 @@ for crash in $CRASH_DIR/id:*; do
     CMD="$BINARY < $crash";
     $BINARY < $crash >/dev/null
     counter=$((counter + 1))
-    
-    # Set ASAN_OPTIONS to log to the desired file
-    # $BINARY < $crash
 done
 
 python3 script.py
